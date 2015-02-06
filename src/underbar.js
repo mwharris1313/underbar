5851f96da04a7b5350be6bf31df35bb8ea9dbf51
Michael Harris <mwharris333@gmail.com>
2015-02-06 17:54:00 -0500

    added reject method

--- src/underbar.js
+++ src/underbar.js
@@ -103,6 +103,7 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item, index){return !test(item, test)});
   };
 
   // Produce a duplicate-free version of the array.
