be212bb35063be85ab8d940af69138284ed6f0ae
Michael Harris <mwharris333@gmail.com>
2015-02-06 18:41:40 -0500

    added map method

--- src/underbar.js
+++ src/underbar.js
@@ -125,6 +125,17 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var result = [];
+    if (collection.length) {
+      for (var i=0; i<collection.length; i++){
+        result.push(iterator(collection[i], i, collection));
+      }
+    } else if (typeof collection === "object") {
+      for (var prop in collection) {
+        result.push(iterator(collection[i], i, collection));
+      }
+    }
+    return result;
   };
 
   /*
