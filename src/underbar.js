d1e65679aeba079f61627e3a640bd2c2ce85ea70
Michael Harris <mwharris333@gmail.com>
2015-02-13 13:48:54 -0500

    added extend method

--- src/underbar.js
+++ src/underbar.js
@@ -253,6 +253,12 @@ _.some = function(collection, iterator) {
 //     bla: "even more stuff"
 //   }); // obj1 now contains key1, key2, key3 and bla
 _.extend = function(obj) {
+	for (var i=1; i < arguments.length; i++){
+		for (var key in arguments[i]) {
+			obj[key] = arguments[i][key];
+		}
+	}
+	return obj;
 };
 
 // Like extend, but doesn't ever overwrite a key that already
