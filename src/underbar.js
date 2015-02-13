b7a4cfe412fbafba0569d20c9d175b42e2959a34
Michael Harris <mwharris333@gmail.com>
2015-02-13 12:06:08 -0500

    fixed reject method , naming of args to test()
    renamed args passed to test in filter function of reject method.
    to map to naming conventions used in test function of original filter method.

--- src/underbar.js
+++ src/underbar.js
@@ -105,7 +105,7 @@ _.filter = function(collection, test) {
 _.reject = function(collection, test) {
 	// TIP: see if you can re-use _.filter() here, without simply
 	// copying code in and modifying it
-	return _.filter(collection, function(item, index){return !test(item, test)});
+	return _.filter(collection, function(item, index){return !test(item, index)});
 };
 
 // Produce a duplicate-free version of the array.
